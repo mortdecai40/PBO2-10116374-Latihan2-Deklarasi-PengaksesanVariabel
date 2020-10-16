ff1cc7e2ab89ba3ee19e81eb01609d9d5fa6fef8
mortdecai40 <rizkinabil.999@gmail.com>
2020-10-16 13:44:41 +0700

    Program Deklarasi

---
+++ src/pbo2/pkg10116374/latihan2/deklarasi/pengaksesanvariabel/PBO210116374Latihan2DeklarasiPengaksesanVariabel.java
@@ -0,0 +1,39 @@
+
+package pbo2.pkg10116374.latihan2.deklarasi.pengaksesanvariabel;
+/**
+ * @author
+ * NAMA                 : TEUKU RIZKI NABIL
+ * KELAS                : PBO2
+ * NIM                  : 10116374
+ * Deskripsi Program    : Program untuk deklarasi & pengaksesan variabel
+ */
+public class PBO210116374Latihan2DeklarasiPengaksesanVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilan Hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variable PHi = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("isi variabel karakter = " + nilaiKarakter);
+        
+                
+    }
+    
+}
